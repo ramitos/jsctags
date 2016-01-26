a87471ee1b291eb0d42b477f4e03afa5d24b1637
Pascal Lalancette <okcompute@me.com>
2016-01-26 06:04:48 -0500

    Make the CLI feels more like the original CTAGS CLI.

--- src/ctags.js
+++ src/ctags.js
@@ -17,8 +17,21 @@ var SPECIAL_FIELDS = {
   parent: true
 };
 
-module.exports = function (tags) {
-  return tags.map(function (tag) {
+module.exports = function (tags, options) {
+  if (!options) {
+    options = {
+      args: true,
+      local: true
+    }
+  }
+  return tags.filter(tag => {
+    if (tag.origin['!data'].isArg && !options.args) {
+      return false
+    } else if ((tag.namespace !== undefined) && !options.local) {
+      return false
+    }
+    return true
+  }).map(tag => {
     var buf = [tag.name, '\t', tags.tagfile, '\t'];
     buf.push(tag.addr !== undefined ? tag.addr : '//');
     var tagfields = [];
